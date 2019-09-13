773add217b76acf67d0f3a1683fbf018cd42d325
MariahRojas <51241518+MariahRojas@users.noreply.github.com>
2019-09-13 10:17:18 -0400

    Update
    Courses show in console!

--- client/src/App.js
+++ client/src/App.js
@@ -10,21 +10,8 @@ class App extends Component {
       <div id="root">
           <div>
             <Header />
-              <ul>
-                <li>1/2 x 3/4 inch parting strip</li>
-                <li>1 x 2 common pine</li>
-                <li>1 x 4 common pine</li>
-                <li>1 x 10 common pine</li>
-                <li>1/4 inch thick lauan plywood</li>
-                <li>Finishing Nails</li>
-                <li>Sandpaper</li>
-                <li>Wood Glue</li>
-                <li>Wood Filler</li>
-                <li>Minwax Oil Based Polyurethane</li>
-              </ul>
-        </div>
-      
-        <Courses />
+            <Courses />
+        </div>   
       </div>
     );
   }
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,22 +1,33 @@
-/* This component provides the "Courses" screen by retrieving the list of courses 
-    from the REST API's /api/courses route and rendering a list of courses. 
-    Each course needs to link to its respective "Course Detail" screen. 
-    This component also renders a link to the "Create Course" screen. */
-
-    // Home route /
-
-        // retrieve list of courses
-            //each course needs a link to course detail.
-        
-        //create course link 
 import React, { Component } from 'react';
 
-    class Courses extends Component {
+class Courses extends Component {
+    constructor() {
+        super()
+        this.state = {
+            allCourses: []
+        }
+    }
+
+    //runs automatically when component is mounted
+    componentDidMount() {
+        fetch('http://localhost:5000/api/courses') //call to back-end
+            .then(res => res.json())
+            .then(allCourses => this.setState({allCourses}, () => console.log('Courses fetched..', allCourses)))
+    }
+
+    render() {
+        return (
+            <div className="bounds">
+                <li>list</li>
+            </div>
+        )
+    }
+}
         
-        render() {
-            return (
-                <div class="bounds">
-                  <div class="grid-33"><a class="course--module course--link" href="course-detail.html">
+export default Courses
+
+
+/*                   <div class="grid-33"><a class="course--module course--link" href="course-detail.html">
                       <h4 class="course--label">Course</h4>
                       <h3 class="course--title">Build a Basic Bookcase</h3>
                     </a></div>
@@ -33,10 +44,15 @@ import React, { Component } from 'react';
                           viewBox="0 0 13 13" class="add">
                           <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
                         </svg>New Course</h3>
-                    </a></div>
-                </div>
-            )
-        }
-    }
+                    </a></div> */
+/* This component provides the "Courses" screen by retrieving the list of courses 
+    from the REST API's /api/courses route and rendering a list of courses. 
+    Each course needs to link to its respective "Course Detail" screen. 
+    This component also renders a link to the "Create Course" screen. */
+
+    // Home route /
+
+        // retrieve list of courses
+            //each course needs a link to course detail.
         
-export default Courses
+        //create course link                    
